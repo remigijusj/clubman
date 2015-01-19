1a23b041f44f2880a2e97cefba7c18c75ff490f0
Remigijus Jodelis <remi@samesystem.com>
2015-01-19 21:10:46 +0200

    deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+replaces ()
+{
+  local name="static.go"
+  local pass="$1"
+  local reload="$2"
+
+  echo "SUBST $name WITH reload=$reload"
+  sed -i -e "s/emailsPass = \".*\"/emailsPass = \"$pass\"/g" -e "s/reloadTmpl = [a-z]*/reloadTmpl = $reload/g" "$name"
+}
+
+git pull
+
+read -e -s -p "Enter the password: " password
+replaces $password "false"
+go build
+replaces "" "true"
+
+./nk-fitness
